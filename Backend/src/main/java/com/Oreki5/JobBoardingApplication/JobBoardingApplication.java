c766ca162dbe785029ccc83e71511ec578ef8a46
Oreki-5 <adityacoc1602@gmail.com>
2026-08-11 16:16:09 +0530

    base sprng boot project

---
+++ Backend/src/main/java/com/Oreki5/JobBoardingApplication/JobBoardingApplication.java
@@ -0,0 +1,13 @@
+package com.Oreki5.JobBoardingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardingApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Oreki5/JobBoardingApplication/JobBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oreki5.JobBoardingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
